8302b6ca43e86007c42b28c639005cf4979fb24b
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-04-05 10:12:18 +0545

    Array Lab 2

--- Array.c
+++ Array.c
@@ -1,16 +1,91 @@
+// 1. WAP in C to store elements in an array and print them.
 #include<stdio.h>
- int main(){
-    int i,a[5],total=0;
-    float per;
-   for ( i=0;i<=4;i++){
-    printf("Enter marks :");
-    scanf("%d",&a[i]);
-   
-    total+=(a[i]);
-    per=total/5;
-   }
-    printf("Total marks obtained = %d\n",total);
-     printf("Percentage = %f",per);
+int main(){
+     int i,n,a[100];
+   printf("Enter number of elements: ");
+   scanf("%d",&n);
+   for ( i=0;i<n;i++){
+    printf("Enter elements:");
+    scanf("%d",&a[i]); 
+} 
+printf("Elements in array :");
+for ( i=0;i<n;i++){  
+printf(" %d\t",a[i]);
+}
+printf("\t");
 return 0;
- }
- 
+}
+
+
+// 2. WAP in C to read n numbers of values in an array and display them in reverse order.
+#include<stdio.h>
+int main(){
+     int i,n,a[100];
+   printf("Enter number of elements: ");
+   scanf("%d",&n);
+   for ( i=0;i<n;i++){
+    printf("Enter elements:");
+    scanf("%d",&a[i]); 
+} 
+printf("Elements in reverse order :");
+for ( i=n-1;i>=0;i--){  
+printf(" %d\t",a[i]);
+}
+printf("\t");
+return 0;
+}
+
+
+// 3.WAP in c to find sum of all elements of the array
+#include<stdio.h>
+int main(){
+   int i,n,a[100],sum=0;
+   printf("Enter number of elements: ");
+   scanf("%d",&n);
+   for ( i=0;i<n;i++){
+    printf("Enter elements:");
+    scanf("%d",&a[i]); 
+    sum+=(a[i]);
+   }
+   printf("Sum of all elements = %d\n",sum);
+return 0;
+}
+
+
+// 4.WAP in c to find average of all elements of the array
+#include<stdio.h>
+int main(){
+   int i,n,a[100],sum=0,avg=0;
+   printf("Enter number of elements: ");
+   scanf("%d",&n);
+   for ( i=0;i<n;i++){
+    printf("Enter elements:");
+    scanf("%d",&a[i]); 
+    sum+=(a[i]);
+   }
+   avg=sum/i;
+   printf("Average of all elements = %d\n",avg);
+return 0;
+}
+
+
+// 5.WAP in C to find the maximum value of the array input by the user.
+#include<stdio.h>
+int main(){
+   int i,n,a[100],max;
+   printf("Enter number of elements: ");
+   scanf("%d",&n);
+   for ( i=0;i<n;i++){
+    printf("Enter elements:");
+    scanf("%d",&a[i]); 
+    max=a[0];
+   }
+   for ( i=0;i<=4;i++){
+      if(a[i]>max)
+      max=a[i];
+   }
+   printf("Maximum Value = %d\n",max);
+return 0;
+}
+
+
