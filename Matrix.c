9a6bb99eaa187c4484d06e41a67204d3af03c585
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-04-06 23:52:24 +0545

    Matrix

--- Matrix.c
+++ Matrix.c
@@ -1,12 +1,14 @@
 // Matrix
  #include<stdio.h>
  int main(){
-    int i,j,m,n,a[m][n],b[m][n];
+    int m,n,i,j;
+    int a[100][100],b[100][100];
+
     printf("Enter number of rows: ");
     scanf("%d",&m);
     printf("Enter number of columns: ");
     scanf("%d",&n);
-
+    
     printf("\nTaking Matrix from User\n");
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
@@ -14,16 +16,26 @@
             scanf("%d",&a[i][j]);
         }
     }
-    // printf("\n");
-    //     for(i=0;i<m;i++){
-    //     for(j=0;j<n;j++){
-    //         printf("Enter matrix B[%d][%d]: ",i,j);
-    //         scanf("%d",&b[i][j]);
-    //     }
-    // }
+    
+    printf("\n");
+        for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("Enter matrix B[%d][%d]: ",i,j);
+            scanf("%d",&b[i][j]);
+        }
+    }
+    printf("\nMatrix A:\n");
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
-            printf("%d\t",a[i][j]);
+           printf("%d\t",a[i][j]);
         }
+        printf("\n");
+    }
+    printf("\nMatrix B:\n");
+        for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d\t",b[i][j]);
+        }
+         printf("\n");
     }
 }
