0426f9e816950dfe31d5f9b21069766d40116288
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-04-21 16:04:28 +0545

    Basic Concepts

--- Basic_Concepts.c
+++ Basic_Concepts.c
@@ -1,51 +1,51 @@
 // //Hello World Program
-// #include<stdio.h>
-// int main(){
-//     printf("Hello world!");
-// return 0;
-// }
+#include<stdio.h>
+int main(){
+    printf("Hello world!");
+return 0;
+}
 
 // // Data Types
-// #include <stdio.h>
-// int main() {
-//     printf("int: %ld \n", sizeof(int));
-//     printf("float: %ld \n", sizeof(float));
-//     printf("double: %ld \n", sizeof(double));
-//     printf("char: %ld \n", sizeof(char));
-// return 0;
-// }
+#include <stdio.h>
+int main() {
+    printf("int: %ld \n", sizeof(int));
+    printf("float: %ld \n", sizeof(float));
+    printf("double: %ld \n", sizeof(double));
+    printf("char: %ld \n", sizeof(char));
+return 0;
+}
 
 // // Inputs & Outputs
 // // 1.Character
-// #include<stdio.h>
-// int main(){
-//     char a=getchar();
-//     printf("You entered: ");
-//     putchar(a);
-//     // The input is stored in variable a.
-// return 0;
-// }
+#include<stdio.h>
+int main(){
+    char a=getchar();
+    printf("You entered: ");
+    putchar(a);
+    // The input is stored in variable a.
+return 0;
+}
 
 // // 2.String
-// #include <stdio.h>
-// int main() {
-//     char a[100];
-//     gets(a); 
-//     printf("You entered:");
-//     puts(a);
-// return 0;
-// }
+#include <stdio.h>
+int main() {
+    char a[100];
+    gets(a); 
+    printf("You entered:");
+    puts(a);
+return 0;
+}
 
 // // 3.Foramt Specifiers
-// #include<stdio.h>
-// int main(){
-//     int a;
-//     float b;
-//     double c;
-//     scanf("%d%f%ld",&a,&b,&c);
-//     printf("You enterd:\n %d\n %3.2f\n %ld",a,b,c);
-// return 0;
-// }
+#include<stdio.h>
+int main(){
+    int a;
+    float b;
+    double c;
+    scanf("%d%f%ld",&a,&b,&c);
+    printf("You enterd:\n %d\n %3.2f\n %ld",a,b,c);
+return 0;
+}
 
 //Operators
 #include<stdio.h>
