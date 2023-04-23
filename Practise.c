39eb1bbad2a1a4ec6bfa423d441e6e2bbfef1a43
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-04-23 14:02:35 +0545

    Practise

--- Practise.c
+++ Practise.c
@@ -1,128 +1,165 @@
-// WAP to show size of variables using "size of"
+// // WAP to show size of variables using "size of"
+// #include <stdio.h>
+// int main() {
+//     int intType;
+//     float floatType;
+//     char charType;
+//     double doubleType;
+
+//     printf("Size of int: %d \n", sizeof(intType));
+//     printf("Size of float: %d \n", sizeof(floatType));
+//     printf("Size of char: %d \n", sizeof(charType));
+//     printf("Size of double: %d \n", sizeof(doubleType));
+
+// return 0;
+// }
+
+// // WAP to convert feet into inch.
+// #include<stdio.h>
+// int main(){
+//     int feet,inch;
+//     printf("Enter the measurement in feet: ");
+//     scanf("%d",&feet);
+//     inch=feet*12;
+//     printf("%d Feet = %d Inch",feet,inch);
+
+// return 0; 
+// }
+
+// // WAP to enter marks of 5 subjects and show total marks and percentage.
+// #include<stdio.h>
+//  int main(){
+//     int c,mth,iit,dl,phy,total;
+//     float per;
+//     printf("Enter marks in C: ");
+//     scanf("%d",&c);
+//     printf("Enter marks in Math: ");
+//     scanf("%d",&mth);
+//     printf("Enter marks in Physics: ");
+//     scanf("%d",&phy);
+//     printf("Enter marks in DL: ");
+//     scanf("%d",&dl);
+//     printf("Enter marks in IIT: ");
+//     scanf("%d",&iit);
+//     total=(c+mth+phy+dl+iit);
+//     per=(total/5);
+//     printf("Total marks obtained = %d\n",total);
+//     printf("Percentage = %f",per);
+//     return 0;
+//  }
+
+
+// // WAP to show whether a number is divisible by 5 and 3 but not 10.
+// #include<stdio.h>
+// int main(){
+//     int n;
+//     printf("Enter a number: ");
+//     scanf("%d",&n);
+//     if(n%5==0 && n%3==0)
+//     {
+//         if(n%10!=0)
+//         printf("%d is divisible by 5 and 3 but not 10",n);
+//         else
+//         printf("%d is divisible by 5,3 and 10",n);
+//     }
+//     else{
+//         printf("%d is not divisible by 5 and 3",n);
+//     }
+//     return 0;
+// }
+
+
+
+// // WAP to enter 2 number and do following operations.
+// // IF-ELSE
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     char op;
+//     printf("Enter the operator (+,-,*,/): ");
+//     scanf("%c",&op);
+//     printf("Enter 2 numbers: ");
+//     scanf("%d%d",&a,&b);
+//     if (op=='+'){
+//         printf("%d + %d = %d",a,b,a+b);
+//     }else if (op=='-'){
+//         printf("%d - %d = %d",a,b,a-b);
+//     }else if (op=='*'){
+//         printf("%d * %d = %d",a,b,a*b);
+//     }else if (op=='/'){
+//         printf("%d / %d = %d",a,b,a/b);
+//     }else{
+//         printf("Invalid Operations");
+//     }
+//     return 0;
+
+// }
+
+
+// // SWITCH CASE
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     char op;
+//     printf("Enter the operator (+,-,*,/): ");
+//     scanf("%c",&op);
+//     printf("Enter 2 numbers: ");
+//     scanf("%d%d",&a,&b);
+//     switch(op){
+//     case '+':
+//         printf("%d + %d = %d",a,b,a+b);
+//         break;
+//     case '-':
+//         printf("%d - %d = %d",a,b,a-b);
+//         break;
+//     case '*':
+//         printf("%d * %d = %d",a,b,a*b);
+//         break;
+//     case '/':
+//         printf("%d / %d = %d",a,b,a/b);
+//         break;
+//     default:
+//         printf("Invalid Operations");
+//        return 0;
+
+// }
+// }
+
+
+// #include <stdio.h>
+
+// int main ()
+// {
+//     int x,y;
+//     printf("Enter two numbers x and y: ");
+//     scanf("%d%d",&x,&y);
+
+// /*x=x^y*/ x=x+y;
+//     /*y=x^y;*/  y=x-y;
+//     /*x=x^y;*/ x=x-y;
+
+//     printf("the value of x is: %d",x);
+//     printf("the value of y is: %d",y);
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
+#include <math.h>
+
 int main() {
-    int intType;
-    float floatType;
-    char charType;
-    double doubleType;
-
-    printf("Size of int: %d \n", sizeof(intType));
-    printf("Size of float: %d \n", sizeof(floatType));
-    printf("Size of char: %d \n", sizeof(charType));
-    printf("Size of double: %d \n", sizeof(doubleType));
-
-return 0;
-}
-
-// WAP to convert feet into inch.
-#include<stdio.h>
-int main(){
-    int feet,inch;
-    printf("Enter the measurement in feet: ");
-    scanf("%d",&feet);
-    inch=feet*12;
-    printf("%d Feet = %d Inch",feet,inch);
-
-return 0; 
-}
-
-// WAP to enter marks of 5 subjects and show total marks and percentage.
-#include<stdio.h>
- int main(){
-    int c,mth,iit,dl,phy,total;
-    float per;
-    printf("Enter marks in C: ");
-    scanf("%d",&c);
-    printf("Enter marks in Math: ");
-    scanf("%d",&mth);
-    printf("Enter marks in Physics: ");
-    scanf("%d",&phy);
-    printf("Enter marks in DL: ");
-    scanf("%d",&dl);
-    printf("Enter marks in IIT: ");
-    scanf("%d",&iit);
-    total=(c+mth+phy+dl+iit);
-    per=(total/5);
-    printf("Total marks obtained = %d\n",total);
-    printf("Percentage = %f",per);
-    return 0;
- }
-
-
-// WAP to show whether a number is divisible by 5 and 3 but not 10.
-#include<stdio.h>
-int main(){
     int n;
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    if(n%5==0 && n%3==0)
-    {
-        if(n%10!=0)
-        printf("%d is divisible by 5 and 3 but not 10",n);
-        else
-        printf("%d is divisible by 5,3 and 10",n);
+    double x, sum = 0;
+
+    printf("Enter the value of x: ");
+    scanf("%lf", &x);
+
+    for (n = 1; n <= 100; n++) {
+        sum += pow(x, -n);
     }
-    else{
-        printf("%d is not divisible by 5 and 3",n);
-    }
+
+    printf("The summation of (x^-n) from N=1 to 100 is: %lf\n", sum);
+
     return 0;
 }
-
-
-
-// WAP to enter 2 number and do following operations.
-// IF-ELSE
-#include<stdio.h>
-int main(){
-    int a,b;
-    char op;
-    printf("Enter the operator (+,-,*,/): ");
-    scanf("%c",&op);
-    printf("Enter 2 numbers: ");
-    scanf("%d%d",&a,&b);
-    if (op=='+'){
-        printf("%d + %d = %d",a,b,a+b);
-    }else if (op=='-'){
-        printf("%d - %d = %d",a,b,a-b);
-    }else if (op=='*'){
-        printf("%d * %d = %d",a,b,a*b);
-    }else if (op=='/'){
-        printf("%d / %d = %d",a,b,a/b);
-    }else{
-        printf("Invalid Operations");
-    }
-    return 0;
-
-}
-
-
-// SWITCH CASE
-#include<stdio.h>
-int main(){
-    int a,b;
-    char op;
-    printf("Enter the operator (+,-,*,/): ");
-    scanf("%c",&op);
-    printf("Enter 2 numbers: ");
-    scanf("%d%d",&a,&b);
-    switch(op){
-    case '+':
-        printf("%d + %d = %d",a,b,a+b);
-        break;
-    case '-':
-        printf("%d - %d = %d",a,b,a-b);
-        break;
-    case '*':
-        printf("%d * %d = %d",a,b,a*b);
-        break;
-    case '/':
-        printf("%d / %d = %d",a,b,a/b);
-        break;
-    default:
-        printf("Invalid Operations");
-       return 0;
-
-}
-}
-
-
--- functions.c
+++ functions.c
@@ -1,67 +1,67 @@
-// WAP to find sum of five numbers entered by user.
-#include<stdio.h>
-int sum(int,int,int,int,int);
-int main(){
-       int a,b,c,d,e;
-       printf("Enter 5 numbers: ");
-       scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-       printf("Sum is %d",sum(a,b,c,d,e));
-       return 0;
-}
-int sum(int a,int b,int c,int d,int e)
-{
-    int add;
-    add=a+b+c+d+e;
-    return add;
-}
+// // WAP to find sum of five numbers entered by user.
+// #include<stdio.h>
+// int sum(int,int,int,int,int);
+// int main(){
+//        int a,b,c,d,e;
+//        printf("Enter 5 numbers: ");
+//        scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+//        printf("Sum is %d",sum(a,b,c,d,e));
+//        return 0;
+// }
+// int sum(int a,int b,int c,int d,int e)
+// {
+//     int add;
+//     add=a+b+c+d+e;
+//     return add;
+// }
 
 
-// WAP creating a function named odd_even to check whether a number is odd or even.
-#include<stdio.h>
-int odd_even(int);
-int main(){
-    int n;
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    odd_even(n);
-    return 0;
-}
-int odd_even(int n)
-{
-    if(n%2==0)
-    printf("%d is even",n);
-    else
-    printf("%d is odd",n);
-}
+// // WAP creating a function named odd_even to check whether a number is odd or even.
+// #include<stdio.h>
+// void odd_even(int);
+// int main(){
+//     int n;
+//     printf("Enter a number: ");
+//     scanf("%d",&n);
+//     odd_even(n);
+//     return 0;
+// }
+// void odd_even(int n)
+// {
+//     if(n%2==0)
+//     printf("%d is even",n);
+//     else
+//     printf("%d is odd",n);
+// }
 
 
-// WAP to check prime numbers or not entered by user.
-#include<stdio.h>
-int prime(int);
-int main(){
-    int n;
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    prime(n);
-    return 0;
-}
-int prime(int n)
-{
-    int i,c=0;
-    for(i=1;i<=n;i++)
-    {
-        if (n%i==0)
-        c++;
-    }
-    if(c==2)
-    printf("%d is prime",n);
-    else
-    printf("%d is composite",n);
-}
+// // WAP to check prime numbers or not entered by user.
+// #include<stdio.h>
+// void prime(int);
+// int main(){
+//     int n;
+//     printf("Enter a number: ");
+//     scanf("%d",&n);
+//     prime(n);
+//     return 0;
+// }
+// void prime(int n)
+// {
+//     int i,c=0;
+//     for(i=1;i<=n;i++)
+//     {
+//         if (n%i==0)
+//         c++;
+//     }
+//     if(c==2)
+//     printf("%d is prime",n);
+//     else
+//     printf("%d is composite",n);
+// }
 
 // WAP to check whether the entered number is palindrome or not with the concept of function
 #include<stdio.h>
-int palindrome(int);
+void palindrome(int);
 int main(){
     int n;
     printf("Enter a number: ");
@@ -69,7 +69,7 @@ int main(){
     palindrome(n);
     return 0;
 }
-int palindrome(int n){
+void palindrome(int n){
     int org,rem,rev=0;
     org = n;
     while(n>0) {
@@ -84,35 +84,35 @@ int palindrome(int n){
 }
 
 
-// WAP to find a factorial of any number enterd by user using function with return type.
-#include<stdio.h>
-int facto(int);
-int main(){
-    int n;
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    printf("Factorial of %d is %d",n,facto(n));
-}
-int facto(int n){
-    if(n<=1)
-    return 1;
-    else
-    return(n*facto(n-1));
-}
+// // WAP to find a factorial of any number enterd by user using function with return type.
+// #include<stdio.h>
+// int facto(int);
+// int main(){
+//     int n;
+//     printf("Enter a number: ");
+//     scanf("%d",&n);
+//     printf("Factorial of %d is %d",n,facto(n));
+// }
+// int facto(int n){
+//     if(n<=1)
+//     return 1;
+//     else
+//     return(n*facto(n-1));
+// }
 
 
-// WAP to convert degree to fahrenheit with use of function.
-#include<stdio.h>
-float convert(float);
-int main(){
-    float celsius;
-    printf("Enter the temperature in Celsius: ");
-    scanf("%f",&celsius);
-    printf("%.2f Celsius = %.2f Fahrenheit",celsius,convert(celsius));
-    return 0;
-}
-float convert(float celsius){
-float fahrenheit;
-     fahrenheit=(celsius*9/5)+32;
-     return fahrenheit; 
-}
+// // WAP to convert degree to fahrenheit with use of function.
+// #include<stdio.h>
+// float convert(float);
+// int main(){
+//     float celsius;
+//     printf("Enter the temperature in Celsius: ");
+//     scanf("%f",&celsius);
+//     printf("%.2f Celsius = %.2f Fahrenheit",celsius,convert(celsius));
+//     return 0;
+// }
+// float convert(float celsius){
+// float fahrenheit;
+//      fahrenheit=(celsius*9/5)+32;
+//      return fahrenheit; 
+// }
