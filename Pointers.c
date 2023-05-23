4c001fe2c256a0f16a236c2f89f84f58e63f161b
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-05-23 17:17:51 +0545

    Pointers

--- Pointers.c
+++ Pointers.c
@@ -1,33 +1,32 @@
-// // Pointers
-// #include<stdio.h>
-// int main(){
-//     int i=3;
-//     int *j;
-//     j=&i;
-//     printf("Address =%d ",j);
-//     printf("Value=%d ",*j);
-// }
+// Pointers
+#include<stdio.h>
+int main(){
+    int i=3;
+    int *j;
+    j=&i;
+    printf("Address =%d ",j);
+    printf("Value=%d ",*j);
+}
 
 // Pointer Array
-// #include <stdio.h>
-// int main() {
-// int i, sum = 0;
-// int x[6]={1,2,4,5,6,7};
+#include <stdio.h>
+int main() {
+int i, sum = 0;
+int x[6]={1,2,4,5,6,7};
 
-//   for(i = 0; i < 6; i++) {
-//     // &x[i] equivalent to x+i representing the address 
-//     // x refers to the first value i.e x[0]
-//     // x+i refers to the upcoming value depending upon i 
-//     // (x+1) refers as x[1], (x+2) refers as x[2]
+  for(i = 0; i < 6; i++) {
+    // &x[i] equivalent to x+i representing the address 
+    // x refers to the first value i.e x[0]
+    // x+i refers to the upcoming value depending upon i 
+    // (x+1) refers as x[1], (x+2) refers as x[2]
 
-//     // Equivalent to sum += x[i]
-//     // *(x+i) representing the value stored in x+i address.
-//       sum += *(x+i);
-//   }
-// printf("Sum = %d", sum);
-// return 0;
-// }
-
+    // Equivalent to sum += x[i]
+    // *(x+i) representing the value stored in x+i address.
+      sum += *(x+i);
+  }
+printf("Sum = %d", sum);
+return 0;
+}
 
 #include<stdio.h>
 int main(){
