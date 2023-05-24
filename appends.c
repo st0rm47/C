64ea2b4f28e5437bc765c61967a649d5ca2d822d
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2023-05-24 15:31:30 +0545

    Append

---
+++ appends.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    char name[100];
+    int dob;
+
+   // "*fp" is a pointer which points to our data file
+    FILE *fp;        
+    
+     //Opens the file in append mode
+    fp = fopen("Test.txt","a");
+
+    // Taking input from the user
+    printf("Enter name:");
+    gets(name);
+    printf("Enter dob: ");
+    scanf("%d",&dob);
+
+    // Prints the data to the existing file 
+    fprintf(fp,"%s\n %d\n",name,dob);
+    
+    // Closes the file
+    fclose(fp);
+}
+
+
